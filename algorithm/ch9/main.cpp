db35af0077a669d9093ae1ec1e2ff9519574bac6
linux_ever <529188712@qq.com>
2016-02-25 22:24:24 +0800

    "添加C++实现图的邻接表表示方法/algorithm/ch9/GraphList.h, main.cpp

---
+++ algorithm/ch9/main.cpp
@@ -0,0 +1,14 @@
+// test for GraphList
+#include "GraphList.h"
+#include <cstdlib>
+
+int main()
+{
+	GraphList graph;
+	graph.createGraph();
+	graph.printGraph();
+
+	system("pause");
+
+	return 0;
+}
