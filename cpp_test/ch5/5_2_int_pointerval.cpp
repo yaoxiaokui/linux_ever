d09596c212b2e069a9260bbede384f74bfbe63dc
linux_ever <529188712@qq.com>
2016-02-28 11:42:57 +0800

    测试int*指针的值，是否为奇数，也就是是否满足4k对齐

---
+++ cpp_test/ch5/5_2_int_pointerval.cpp
@@ -0,0 +1,20 @@
+/*************************************************************************
+	> File Name: 5_2_int_pointerval.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年02月28日 星期日 11时31分07秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n = 100;
+    int * pn = &n;
+    cout << "*pn = " << *pn << ", pn = " << pn << endl;
+
+
+    return 0;
+}
