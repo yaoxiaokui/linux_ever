d3993f621ee1f179dc090e0a4deddcc710e11970
linux_ever <529188712@qq.com>
2016-03-05 17:45:43 +0800

    用面向过程的思想求圆的面积

---
+++ cpp_test/day01/circle_1.cpp
@@ -0,0 +1,25 @@
+/*************************************************************************
+	> File Name: circle_1.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2016年03月05日 星期六 17时29分08秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double r = 0;
+    double s = 0;
+
+    cout << "输入半径：";
+    cin >> r;
+
+    s = 3.14*r*r;
+    cout << "圆的面积：" << s << endl;
+
+
+    return 0;
+}
