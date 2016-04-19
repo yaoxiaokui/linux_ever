a64326deedd2c4bf3e921622c3ce143dd9e45609
linux_ever <529188712@qq.com>
2016-04-19 20:37:58 +0800

    修改二分查找的递归实现，并添加非递归实现版本,主要解决找不到指定值的时候返回值

--- algorithm/ch7/binarySearch.cpp
+++ algorithm/ch7/binarySearch.cpp
@@ -14,17 +14,43 @@ using namespace std;
 
 
 //二分查找
+//递归实现
 int binarySearch(vector<int> & v, int first, int last, int value)
 {
-    int half = (first+last)/2;
+
+    if(first > last)//注意该递归函数的跳出判断语句
+        return -1;
+
+    //int half = (first+last)/2;
+    int half = (last-first)/2 + first;
 
     if(v[half] == value)
         return half;
     else if(v[half] < value)
-        return binarySearch(v, half, last, value);
-    else
-        return binarySearch(v, 0, half, value);
+        return binarySearch(v, half+1, last, value);
+    else if(v[half] > value)
+        return binarySearch(v, first, half-1, value);
 
+}
+
+//非递归实现
+int binarySearch(vector<int> & v, int value)
+{
+    int low = 0, high = v.size()-1;
+
+    while(low <= high){
+        int mid = (low+high)/2;//(high-low)/2 + low
+        //int mid = (high-low)/2 + low;//这个可以防止溢出
+
+        if(v[mid] == value)
+            return mid;
+        else if(v[mid] > value)
+            high = mid-1;
+        else
+            low = mid+1;
+    }
+
+    return -1;
 }
 
 
@@ -49,6 +75,16 @@ int main()
 
     int index = binarySearch(v, 0, v.size()-1, val);
     cout << val << " index is " << index << endl;
+
+    index = binarySearch(v, 0, v.size()-1, 5);
+    cout << 5 << " index is " << index << endl;
+
+    index = binarySearch(v, 6);
+    cout << 6 << " index is " << index << endl;
+
+    index = binarySearch(v, 100);
+    cout << 100 << " index is " << index << endl;
+
 
 
     return 0;
