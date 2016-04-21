511d5dc460ea2145cc5ee28e2cb24507e75f796e
linux_ever <529188712@qq.com>
2016-04-21 22:12:58 +0800

    修改了昨天的父子进程通过管道进行通信的实例

--- process_thread/ch4/mainpipe.c
+++ process_thread/ch4/mainpipe.c
@@ -19,18 +19,76 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-//服务器从rfd中读取文件的名字，向wrd中写入文件的内容
-void server(int rfd, int wrd)
+//服务器从rfd中读取文件的名字，向wfd中写入文件的内容
+void server(int rfd, int wfd)
 {
+    char fileName[1024];
+    char fileContent[2048];
+
+    memset(fileName, 0, 1024);
+    memset(fileContent, 0, 2048);
+
+    //从rfd中读取文件的名字，
+    int n = read(rfd, fileName, 1024);
+    fileName[n] = 0;
+    printf("server receive the file name is %s\n", fileName);
+
+    int filefd = open(fileName, O_RDONLY);//打开文件
+    if(filefd < 0){
+        printf("open error\n");
+        return;
+    }
+    else{//读取文件的内容，并写入到wfd中
+         //读取文件的内容到fileContent中
+         int num = 0;
+         while((num = read(filefd, fileContent, 2048)) > 0){
+            printf("server read the fileContent is: %s", fileContent);
+            //将fileContent中的内容写入到wfd中
+            write(wfd, fileContent, num);
+        }
+    }
+
+    close(filefd);
+    close(rfd);
+    close(wfd);
 
 }
-//客户端从rfd中读取文件的内容，向wrd中写入文件的名字
-void client(int rfd, int wrd)
+
+//客户端从rfd中读取文件的内容，向wfd中写入文件的名字
+void client(int rfd, int wfd)
 {
+    char fileName[1024];
+    char fileContent[2048];
+
+    memset(fileName, 0, 1024);
+    memset(fileContent, 0, 2048);
     
+    printf("输入文件名字：");
+    //从标准输入输入文件的名字
+    fgets(fileName, 1024, stdin);
+
+    int len = strlen(fileName);
+    if(fileName[len-1] == '\n')
+        len--;
+
+    //向wfd中写入文件的名字
+    write(wfd, fileName, len);
+    printf("fileName = %s\n", fileName);
+    //从rfd中读取文件的内容
+    int n;
+    while((n = read(rfd, fileContent, 2048)) > 0){
+        printf("client receive the content is: %s", fileContent);
+    }
+
+    close(rfd);
+    close(wfd);
 }
 
+//主函数
 int main()
 {
 
@@ -67,6 +125,7 @@ int main()
         close(pipe2[1]);
         client(pipe2[0], pipe1[1]);
     }
+
 
     waitpid(child_pid, NULL, 0);//等待子进程退出
     return 0;
