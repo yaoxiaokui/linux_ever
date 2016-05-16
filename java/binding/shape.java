49ae727f15737c1c471a046c8f93d8a41b7f6a2d
yaoxiaokui <yaoxiaokui@163.com>
2016-05-16 20:32:57 +0800

    测试java中的动态绑定现象

---
+++ java/binding/shape.java
@@ -0,0 +1,68 @@
+/*************************************************************************
+	> File Name: shape.java
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 16 May 2016 03:29:08 PM CST
+ ************************************************************************/
+
+public class shape
+{
+    public static void main(String [] args){
+        Circle c1 = new Circle();
+        c1.draw();
+
+        Rectange r1 = new Rectange();
+        r1.draw();
+
+        Shape s1 = new Circle();
+        s1.draw();
+        Shape s2 = new Rectange();
+        s2.draw();
+
+        System.out.println("Person draw: ");
+        Person p = new Person();
+        p.drawShape(c1);
+        p.drawShape(r1);
+        //p.drawShape(s1);
+        //p.drawShape(s2);
+        //
+        Shape s3 = p.getCircleShape();
+        s3.draw();
+    }
+}
+
+class Person
+{
+    void drawShape(Shape shape){
+        shape.draw();
+    }
+
+    Shape getCircleShape(){
+        return new Circle();
+    }
+}
+
+//基类
+class Shape
+{
+    void draw(){
+        System.out.println("Shape.draw()...");
+    }
+
+}
+
+//圆类
+class Circle extends Shape
+{
+    void draw(){
+        System.out.println("Circle.draw()...");
+    }
+}
+
+//矩形类
+class Rectange extends Shape
+{
+    void draw(){
+        System.out.println("Rectange.draw()...");
+    }
+}
