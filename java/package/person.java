f0c69f0dd09dbc438f780b99d292a7180108b09f
yaoxiaokui <yaoxiaokui@163.com>
2016-05-16 20:34:11 +0800

    测试包的作用，并使用包

---
+++ java/package/person.java
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: person.java
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 16 May 2016 04:14:59 PM CST
+ ************************************************************************/
+
+package person;
+
+public class person
+{
+    void print(){
+        System.out.println("person.print()....");
+    }
+}
+
---
+++ java/package/testPerson.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: testPerson.java
+	> Author: 
+	> Mail: 
+	> Created Time: Mon 16 May 2016 04:16:22 PM CST
+ ************************************************************************/
+
+public class testPerson
+{
+    public static void main(String[] args){
+        person p = new person();
+    }
+}
+
